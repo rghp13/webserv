bf51c2c1710300d17f9aa0f2f03ab50a51535b4a
Dimitri SCRIABINE <dscriabi@e1r6p20.clusters.42paris.fr>
2022-09-06 17:55:38 +0200

    created Process_Fork.cpp

---
+++ srcs/Process_Fork.cpp
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Process_Fork.cpp                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dscriabi <dscriabi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/06 17:35:18 by dscriabi          #+#    #+#             */
+/*   Updated: 2022/09/06 17:46:47 by dscriabi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/webserv.hpp"
+
+Answer	fork_request(Request request)
+{
+	Answer	retval;
+
+	//some code
+	return (retval);
+}
