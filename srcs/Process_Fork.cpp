8cde2fea03de608a6e0ea1d8fdf4b9df8d85ab02
Dimitri SCRIABINE <dscriabi@e2r11p10.clusters.42paris.fr>
2022-09-09 15:53:16 +0200

    moved config file check earlier to check if method is valid

--- srcs/Process_Fork.cpp
+++ srcs/Process_Fork.cpp
@@ -6,7 +6,7 @@
 /*   By: dscriabi <dscriabi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/06 17:35:18 by dscriabi          #+#    #+#             */
-/*   Updated: 2022/09/09 14:44:50 by dscriabi         ###   ########.fr       */
+/*   Updated: 2022/09/09 15:50:39 by dscriabi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,8 @@
 
 Answer	fork_request(Request request, std::vector<conf> Vconf)
 {
-	Answer	retval;
+	Answer						retval;
+	std::vector<conf>::iterator	current_conf;
 
 	if (request._malformed) //Malformed Request
 	{
@@ -34,23 +35,52 @@ Answer	fork_request(Request request, std::vector<conf> Vconf)
 		std::cout << "Error 414 on Request" << std::endl;
 		return (retval);
 	}
+
+	//Get the config that matches the socket this request came from
+	current_conf = strict_scan(Vconf, request);
+	if (current_conf == Vconf.end())
+	{
+		current_conf = non_strict_scan(Vconf, request);
+		if (current_conf == Vconf.end())
+		{
+			retval.SetStatus(HTTP_ERR_500);
+			return (retval);
+		}
+	}
+	//
+	
 	if (request._Method == "GET")
 	{
 		//commit GET processing
+		if ((current_conf->get_Method()&GET) != GET)
+		{
+			retval.SetStatus(HTTP_ERR_405);
+			return (retval);
+		}
 		std::cout << "accepted as GET" << std::endl;
-		retval = process_get(request, Vconf);
+		retval = process_get(request, current_conf);
 		return (retval);
 	}
 	if (request._Method == "POST")
 	{
 		//commit POST processing
+		if ((current_conf->get_Method()&POST) != POST)
+		{
+			retval.SetStatus(HTTP_ERR_405);
+			return (retval);
+		}
 		return (retval);
 	}
 	if (request._Method == "DELETE")
 	{
 		//commit DELETE processing
+		if ((current_conf->get_Method()&DELETE) != DELETE)
+		{
+			retval.SetStatus(HTTP_ERR_405);
+			return (retval);
+		}
 		std::cout << "accepted as DELETE" << std::endl;
-		retval = process_delete(request, Vconf);
+		retval = process_delete(request, current_conf);
 		return (retval);
 	}
 	retval.SetStatus(HTTP_ERR_501);
