5fa874935be3fe37759e53bbb35f594e19b74678
Dimitri Scriabine <scriabine.dimitri@gmail.com>
2022-10-05 05:14:14 +0200

    I am litterally stupid

--- srcs/CGIManager.cpp
+++ srcs/CGIManager.cpp
@@ -6,7 +6,7 @@
 /*   By: dimitriscr <dimitriscr@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/13 15:11:06 by dimitriscr        #+#    #+#             */
-/*   Updated: 2022/10/04 20:38:20 by dimitriscr       ###   ########.fr       */
+/*   Updated: 2022/10/05 05:13:57 by dimitriscr       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,8 +41,8 @@ void    CGIManager::initEnv()
 	_env["SERVER_PROTOCOL"] = HTTP_VERS;
 	_env["SERVER_PORT"] = SSTR(_config.get_Port());
 	_env["REQUEST_METHOD"] = _request._Method;
-	_env["SCRIPT_NAME"] = get_ressource_location(_location, _request._Path);//_request._Path.replace(0, _location._path.size(), _location._root); //see here
-	_env["PATH_INFO"] = _env["SCRIPT_NAME"];
+	_env["SCRIPT_NAME"] = get_ressource_location(_location, _request._Path);
+	_env["PATH_INFO"] = _request._Path; //this needs to be the exact full path of the script, probably use getcwd()
 	_env["DOCUMENT_ROOT"] = _location._root;
 	_env["QUERY_STRING"] = _request._Query; //see here
 	//probably needs authentification headers
@@ -68,15 +68,21 @@ std::string CGIManager::runCGI( void )
 {
     pid_t           pid;
     int             sendpipe[2];
-    int             recvpipe[2];
+	int             recvpipe[2];
     char            **env;
-    std::string     retstr;
+	std::string		retstr;
 
     env = this->charArray();
 
+	char *args[] = {
+		(char*)_location._cgi.second.c_str(),
+		(char*)_env["SCRIPT_NAME"].c_str(),
+		NULL
+	};
+
     if (pipe(sendpipe))
 		return ("Error Status 500");
-    if (pipe(recvpipe))
+	if (pipe(recvpipe))
 	{
 		close(sendpipe[0]);
 		close(sendpipe[1]);
@@ -95,12 +101,7 @@ std::string CGIManager::runCGI( void )
     }
     else if (!pid)
     {
-		char *args[] = {
-			(char*)_location._cgi.second.c_str(),
-			(char*)_env["SCRIPT_NAME"].c_str(),
-			NULL
-		};
-        //created process
+        close(sendpipe[1]);
 		dup2(sendpipe[0], STDIN_FILENO);
 		dup2(recvpipe[1], STDOUT_FILENO);
 		execve((_location._cgi.second).c_str(), args, env);
@@ -113,39 +114,25 @@ std::string CGIManager::runCGI( void )
 		//write what needs to be written to sendpipe[1]
 		std::cout << "Started writting to CGI" << std::endl;
 		if (_request._Body.size())
-			write(sendpipe[1], _request._Body.c_str(), 10);//_request._Body.size());
+			write(sendpipe[1], _request._Body.c_str(), 10); //_request._Body.size());
 		close(sendpipe[1]);
 		std::cout << "Finished writting to CGI" << std::endl;
 		
-		waitpid(-1, NULL, 0);
+		waitpid(pid, NULL, 0);
 
 		std::cout << "CGI has ended" << std::endl;
+
 		char	buffer[1025] = {0};
 		int		ret			 = 1024;
-		fd_set	read_fds;
-		timeval	timeout;
-		
-		FD_SET(recvpipe[0], &read_fds);
-		timeout.tv_sec = 1;
-		timeout.tv_usec = 0;
-		while (select(recvpipe[0] + 1, &read_fds, NULL, NULL, &timeout) != 0)
+
+		while (ret == 1024)
 		{
-			if (FD_ISSET(recvpipe[0], &read_fds) == 0)
-				break;
-			std::cout << "." << std::endl;
 			ret = read(recvpipe[0], buffer, 1024);
-			std::cout << ". but after the read this time" << std::endl;
-			if (ret == 0)
-				break;
 			buffer[ret] = '\0';
 			retstr += buffer;
 		}
-		std::cout << "Finished reading from CGI" << std::endl;
-    }
-
+	}
 	close(sendpipe[0]);
-	close(recvpipe[0]);
-	close(recvpipe[1]);
 
 	for (size_t i = 0; env[i]; i++)
 		delete[] env[i];
